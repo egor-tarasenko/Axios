da1b1393b41d847ca2662f036a78c738986bec46
Egor Tarasenko <tarasenkoe324@gmail.com>
2023-12-26 21:12:32 +0200

    Axios

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+    this.fun()
+  },
+  methods: {
+    fun() {
+      axios.get('https://api.giphy.com/v1/gifs/trending?api_key=O0WPlsebm26FrZQELgaSjvxRcP5cbQvf')
+          .then((response) => {
+            console.log(response.data); // Выводим данные в консоль
+          })
+          .catch((error) => {
+            console.error('Ошибка', error)
+          })
+    },
+  },
+}
+</script>
